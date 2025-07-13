1341123caff6501eecd2ed50d5dbf852aab88681
xbzl <138094278+xbzl@users.noreply.github.com>
2025-07-13 14:46:30 +0800

    Update wuming.js

--- Widgets/wuming.js
+++ Widgets/wuming.js
@@ -1,11 +1,12 @@
 var WidgetMetadata = {
   id: "ti.bemarkt.vod.maccms",
   title: "VOD",
-  description: "根据 Keyword 获取 VOD 影视数据",
+  description: "根据 Keyword,获取 VOD 影视数据",
   author: "无名",
   site: "https://raw.githubusercontent.com/xbzl/Forward/refs/heads/main/Widgets",
   version: "1.0.0",  // 更新版本号
   requiredVersion: "0.0.1",
+  illustrate:"致谢",
   modules: [
     {
       title: "获取视频列表",
