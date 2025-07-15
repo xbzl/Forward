0dcb1c0e6e5ad90928267a37e65688317a9fb207
xbzl <138094278+xbzl@users.noreply.github.com>
2025-07-15 09:53:23 +0800

    Update wuming.js

--- Widgets/wuming.js
+++ Widgets/wuming.js
@@ -105,10 +105,6 @@ var WidgetMetadata = {
               title: "先锋资源",
               value: "http://60.204.225.89:1122/api.php/provide/vod/",
             },
-            {
-              title: "低端影视",
-              value: "http://ddys.pro/",
-            },
           ],
         },
         {
@@ -239,10 +235,6 @@ var WidgetMetadata = {
           {
             title: "LSP",
             value: "https://apilsbzy1.com/api.php/provide/vod",
-          },
-          {
-              title: "低端影视",
-              value: "http://ddys.pro/",
           },
         ],
       },
@@ -770,8 +762,7 @@ async function multiSourceSearch(params = {}) {
     "https://caiji.dbzy.tv/api.php/provide/vod/at/josn/",
     "https://alivod.com/api.php/provide/vod/",
     "https://apilsbzy1.com/api.php/provide/vod",
-    "http://60.204.225.89:1122/api.php/provide/vod/",
-    "http://ddys.pro/"
+    "http://60.204.225.89:1122/api.php/provide/vod/"
   ];
   
   // 用于存储所有搜索结果
